aed3326913a7c348e04fc141e64ff687ef5f814e
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-02-05 10:49:34 -0300

    :rocket: initial commit

---
+++ lib/recursao.ex
@@ -0,0 +1,18 @@
+defmodule Recursao do
+  @moduledoc """
+  Documentation for `Recursao`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Recursao.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
